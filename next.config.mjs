2d2a8b72175653b01b455525fdb6da89d6dc7e89
Rebecca Deprey <453900+rdeprey@users.noreply.github.com>
2025-12-06 15:13:08 -0800

    more build fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/view-transition-next-app',
+};
 
 export default nextConfig;
